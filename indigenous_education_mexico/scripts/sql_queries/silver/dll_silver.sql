2199aad8ec80b20e08c2c0c2bdf8dcd91f0e88e3
Mauricio_Ramirez_11 <106550939+Maurici-oh@users.noreply.github.com>
2025-07-15 19:45:54 +0200

    Add files via upload

---
+++ indigenous_education_mexico/scripts/sql_queries/silver/dll_silver.sql
@@ -0,0 +1,92 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables (INEGI_2020)
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+
+	Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+IF OBJECT_ID ('silver.location', 'U') IS NOT NULL -- U is for user created table
+	DROP TABLE silver.location;
+GO
+
+CREATE TABLE silver.location (
+	ENTIDAD			INT,
+	NOM_ENT			NVARCHAR(50),
+	region			NVARCHAR(50),
+	MUN				INT,
+	NOM_MUN			NVARCHAR(100),
+	LOC				INT,
+	NOM_LOC			NVARCHAR(100),
+	LONGITUD		FLOAT,
+	LATITUD			FLOAT,
+	locality_index	INT
+);
+
+GO
+
+IF OBJECT_ID ('silver.population', 'U') IS NOT NULL
+	DROP TABLE silver.population;
+GO
+
+CREATE TABLE silver.population (
+	ENTIDAD			INT,
+	MUN				INT,
+	LOC				INT,
+	POBTOT			INT,
+	POBFEM			INT,
+	POBMAS			INT,
+	P_0A2			INT,
+	P_0A4			INT,
+	P_3YMAS			INT,
+	P_5YMAS			INT, 
+	P_15YMAS		INT,
+	P_18YMAS		INT,
+	locality_index	INT
+);
+GO
+
+IF OBJECT_ID ('silver.indigenous', 'U') IS NOT NULL
+	DROP TABLE silver.indigenous;
+GO
+
+CREATE TABLE silver.indigenous (
+	ENTIDAD			INT,
+	MUN				INT,
+	LOC				INT,
+	PHOG_IND		INT,
+	P3YM_HLI		INT,
+	P3HLINHE		INT,
+	P3HLIHE			INT,
+	P5_HLI			INT,
+	P5_HLI_NHE		INT,
+	P5_HLI_HE		INT,
+	locality_index	INT,
+);
+GO
+
+IF OBJECT_ID ('silver.education', 'U') IS NOT NULL
+	DROP TABLE silver.education;
+GO
+
+CREATE TABLE silver.education (
+	ENTIDAD			INT,
+	MUN				INT,
+	LOC				INT,
+	P15YM_AN		INT,
+	P15YM_SE		INT,
+	P15PRI_IN		INT,
+	P15PRI_CO		INT,
+	P15SEC_IN		INT,
+	P15SEC_CO		INT,
+	P18YM_PB		INT,
+	GRAPROES		FLOAT,
+	GRAPROES_F		FLOAT,
+	GRAPROES_M		FLOAT,
+	locality_index	INT
+);
+GO
---
+++ indigenous_education_mexico/scripts/sql_queries/silver/proc_load_silver.sql
@@ -0,0 +1,215 @@
+/*
+===============================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver) - INEGI_2020
+===============================================================================
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC silver.load_silver;
+===============================================================================
+*/
+
+CREATE OR ALTER PROCEDURE silver.load_silver AS
+BEGIN
+    DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+    BEGIN TRY
+        SET @batch_start_time = GETDATE();
+        PRINT '================================================';
+        PRINT 'Loading Silver Layer';
+        PRINT '================================================';
+
+		--PRINT '------------------------------------------------';
+		--PRINT 'Loading Tables: location';
+		--PRINT '------------------------------------------------';
+
+		-- Loading silver.location
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.location';
+		TRUNCATE TABLE silver.location;
+		PRINT '>> Inserting Data Into: silver.location';
+		INSERT INTO silver.location (
+			ENTIDAD,
+			NOM_ENT,
+			region,
+			MUN,
+			NOM_MUN,
+			LOC,
+			NOM_LOC,
+			LONGITUD,
+			LATITUD,
+			locality_index
+		)
+		SELECT
+			ENTIDAD,
+			NOM_ENT,
+			CASE 
+				WHEN ENTIDAD IN (2, 3, 25, 26) THEN 'Northwest'
+				WHEN ENTIDAD IN (8, 5, 10, 19, 28) THEN 'Northeast'
+				WHEN ENTIDAD IN (18, 14, 6, 16) THEN 'West'
+				WHEN ENTIDAD IN (1, 11, 22, 24, 32) THEN 'Midwest'
+				WHEN ENTIDAD IN (9, 15, 13, 17, 21, 29) THEN 'Center'
+				WHEN ENTIDAD IN (30, 27) THEN 'East'
+				WHEN ENTIDAD IN (12, 20, 7) THEN 'South'
+				WHEN ENTIDAD IN (4, 23, 31) THEN 'Southeast'
+				ELSE 'Total'
+			END AS region,
+			MUN,
+			NOM_MUN,
+			LOC,
+			NOM_LOC,
+			LONGITUD,
+			LATITUD,
+			ROW_NUMBER() OVER (ORDER BY ENTIDAD, MUN, LOC) AS locality_index
+		FROM bronze.location
+		WHERE LOC NOT IN (0, 9999, 9998)
+		ORDER BY ENTIDAD, MUN, LOC;
+		SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> ---------------------------------------------';
+
+		-- Loading silver.population
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.population';
+		TRUNCATE TABLE silver.population;
+		PRINT '>> Inserting Data Into: silver.population';
+		INSERT INTO silver.population (
+			ENTIDAD,
+			MUN,
+			LOC,
+			POBTOT,
+			POBFEM,
+			POBMAS,
+			P_0A2,
+			P_0A4,
+			P_3YMAS,
+			P_5YMAS,
+			P_15YMAS,
+			P_18YMAS,
+			locality_index
+		)
+		SELECT
+			ENTIDAD,
+			MUN,
+			LOC,
+			POBTOT,
+			POBFEM,
+			POBMAS,
+			P_0A2,
+			P_0A4,
+			P_3YMAS,
+			P_5YMAS, 
+			P_15YMAS,
+			P_18YMAS,
+			ROW_NUMBER() OVER (ORDER BY ENTIDAD, MUN, LOC) AS locality_index
+		FROM bronze.population
+		WHERE LOC NOT IN (0, 9999, 9998)
+		ORDER BY ENTIDAD, MUN, LOC;
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> ---------------------------------------------';
+
+        -- Loading silver.indigenous
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.indigenous';
+		TRUNCATE TABLE silver.indigenous;
+		PRINT '>> Inserting Data Into: silver.indigenous';
+		INSERT INTO silver.indigenous (
+			ENTIDAD,
+			MUN,
+			LOC,
+			PHOG_IND,
+			P3YM_HLI,
+			P3HLINHE,
+			P3HLIHE,
+			P5_HLI,
+			P5_HLI_NHE,
+			P5_HLI_HE,
+			locality_index
+		)
+		SELECT
+			ENTIDAD,
+			MUN,
+			LOC,
+			PHOG_IND,
+			P3YM_HLI,
+			P3HLINHE,
+			P3HLIHE,
+			P5_HLI,
+			P5_HLI_NHE,
+			P5_HLI_HE,
+			ROW_NUMBER() OVER (ORDER BY ENTIDAD, MUN, LOC) AS locality_index
+		FROM bronze.indigenous
+		WHERE LOC NOT IN (0, 9999, 9998)
+		ORDER BY ENTIDAD, MUN, LOC;
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> ---------------------------------------------';
+
+        -- Loading silver.education
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.education';
+		TRUNCATE TABLE silver.education;
+		PRINT '>> Inserting Data Into: silver.education';
+		INSERT INTO silver.education (
+			ENTIDAD,
+			MUN,
+			LOC,
+			P15YM_AN,
+			P15YM_SE,
+			P15PRI_IN,
+			P15PRI_CO,
+			P15SEC_IN,
+			P15SEC_CO,
+			P18YM_PB,
+			GRAPROES,
+			GRAPROES_F,
+			GRAPROES_M,
+			locality_index
+		)
+		SELECT
+			ENTIDAD,
+			MUN,
+			LOC,
+			P15YM_AN,
+			P15YM_SE,
+			P15PRI_IN,
+			P15PRI_CO,
+			P15SEC_IN,
+			P15SEC_CO,
+			P18YM_PB,
+			GRAPROES,
+			GRAPROES_F,
+			GRAPROES_M,
+			ROW_NUMBER() OVER (ORDER BY ENTIDAD, MUN, LOC) AS locality_index
+		FROM bronze.education
+		WHERE LOC NOT IN (0, 9999, 9998)
+		ORDER BY ENTIDAD, MUN, LOC;
+	    SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> ---------------------------------------------';
+
+		SET @batch_end_time = GETDATE();
+		PRINT '================================================'
+		PRINT 'Loading Silver Layer is Completed';
+        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '================================================'
+		
+	END TRY
+	BEGIN CATCH
+		PRINT '================================================'
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '================================================'
+	END CATCH
+END
